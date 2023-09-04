ea014f4bebe2a5a85cd1d2c6ebc5fe5ad0d63de8
dude121006 <95970549+dude121006@users.noreply.github.com>
2023-09-05 00:50:20 +0530

    changed input system and added graphing lines

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ totalWidth = width + extraWidth
 oriX, oriY = width/2, height/2
 
 
+pixPerGrid = 25
 font = pygame.font.SysFont('Verdana', 16)
 font2 = pygame.font.SysFont('Serif', 24)
 
@@ -19,90 +20,96 @@ blue = (81, 66, 179)
 black = (0, 0, 0)
 pink = (147, 80, 171)
 green = (76, 168, 100)
+transparent = (0, 0, 0)
 
 
 #Creating windows and surfaces
-win = pygame.display.set_mode((width + extraWidth, height))
+win = pygame.display.set_mode((width + extraWidth, height), )
 pygame.display.set_caption("My advanced grapher")
-win.fill(black)
+win.fill(white)
 
 graph = pygame.Surface((width, height))
 graph.fill(white)
 
-def DrawLine(coord, orientation, color = green, thickness = 2):
-    if orientation.lower() == 'v':
+
+def DrawLine(coord, slope, color = green, thickness = 3):
+
+    if slope.lower() == 'v':
         pygame.draw.line(graph, color, (coord, 0), (coord, height), thickness)
-    elif orientation.lower() == 'h':
+        
+    elif slope.lower() == 'h':
         pygame.draw.line(graph, color, (0, coord), (totalWidth, coord), thickness)
 
-
-
-def GraphPaper(k):
+def DrawGraph(k):
     #makes the mentioned space editable
-    graph.set_clip(0, 0, width, height)
+    win.set_clip(0, 0, width, height)
 
     #displaying the graph paper
     for i in range(math.floor(width / k)):
         gridX = k * i
         gridY = k * i
-        pygame.draw.line(graph, blue, (gridX, 0), (gridX, height), 1) #Vertical lines
-        pygame.draw.line(graph, blue, (0, gridY), (width, gridY), 1)
+        pygame.draw.line(win, blue, (gridX, 0), (gridX, height), 1) #Vertical lines
+        pygame.draw.line(win, blue, (0, gridY), (width, gridY), 1)
 
     #x and y axes
     midX, midY = width/(2), height/(2)
-    # pygame.draw.line(win, black, (midX, 0), (midX, height), 3)
-    # pygame.draw.line(win, black, (0, midY), (width, midY), 3)
-    DrawLine(oriX, 'v', black, 3)
-    DrawLine(oriY, 'h', black, 3)
+
+    pygame.draw.line(win, black, (midX, 0), (midX, height), 3)
+    pygame.draw.line(win, black, (0, midY), (width, midY), 3)
+    # DrawLine(oriX, 'v', black, 3)
+    # DrawLine(oriY, 'h', black, 3)
 
     #border splitting the two sections
     pygame.draw.line(win, black, (width-1, 0), (width-1, height), 1 ) 
 
+    win.set_clip(None)
 
-    graph.set_clip(None)
+def RenderEquations(equations):
+    #updating screen
+        win.set_clip(width + 10, height - 50, width + extraWidth, height)
+        win.fill(white)
 
+        #Join eqn array without commams
+        eqn = ' '.join(equations)
+        #eqn = str.replace(eqn, " ", "")
 
+        #rendering the equations
+        eqnshow = font.render(" y = " + eqn, 1, black)
+        win.blit(eqnshow, (width + 30, height - 50))
 
-def main():
-    active = True
-    done = False
+        win.set_clip(None)
 
-    # Pixels per grid
-    pixPerGrid = 25
-    GraphPaper(pixPerGrid)
+        return eqn
+
+def RenderInstructions():
 
     #Instructions
-    win.blit(graph, (0,0))
-
     title = font2.render("Grapher", 1, pink)
     win.blit(title, (width + 10, 20))
  
     instruct = font.render("Enter the equation: ", 1, black)
     win.blit(instruct, (width + 15, 70))
 
+def main():
+    active = True
+    done = False
+
+    RenderInstructions()
+
     #Equations
     equations = []
-
-    DrawLine(25, 'v')
-    DrawLine(250, 'h')    
 
     #Active loop
     while active:
         #update loop
         pygame.display.update()
  
-        #updating screen
-        win.set_clip(width + 10, height - 50, width + extraWidth, height)
-        win.fill(white)
+        win.blit(graph, (0,0))
 
-        #Join eqn array without commams
-        eqn = ''
-        eqn = eqn.join(equations)
-        eqn = str.replace(eqn, " ", "")
+        eqn = RenderEquations(equations)
+        DrawGraph(pixPerGrid)
 
-        #rendering the equations
-        eqnshow = font.render(" y = " + eqn, 1, black)
-        win.blit(eqnshow, (width + 30, height - 50))
+        graph.fill(white)
 
         # Handling events
         for event in pygame.event.get():
@@ -112,43 +119,45 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 
                 #number keys
-                if event.key == pygame.K_0:
+                if event.key == pygame.K_KP_0:
                     equations.append("0")
-                elif event.key == pygame.K_1:
+                elif event.key == pygame.K_KP_1:
                     equations.append("1")
-                elif event.key == pygame.K_2:
+                elif event.key == pygame.K_KP_2:
                     equations.append("2")
-                elif event.key == pygame.K_3:
+                elif event.key == pygame.K_KP_3:
                     equations.append("3")
-                elif event.key == pygame.K_4:
+                elif event.key == pygame.K_KP_4:
                     equations.append("4")
-                elif event.key == pygame.K_5:
+                elif event.key == pygame.K_KP_5:
                     equations.append("5")
-                elif event.key == pygame.K_6:
+                elif event.key == pygame.K_KP_6:
                     equations.append("6")
-                elif event.key == pygame.K_7:
+                elif event.key == pygame.K_KP_7:
                     equations.append("7")
-                elif event.key == pygame.K_8:
+                elif event.key == pygame.K_KP_8:
                     equations.append("8")
-                elif event.key == pygame.K_9:
+                elif event.key == pygame.K_KP_9:
                     equations.append("9")
+                elif event.key == pygame.K_BACKSPACE:
+                    equations.pop()
 
                 #unicode keys
-                if event.unicode == u'*':
+                if event.key == pygame.K_KP_MULTIPLY:
                     equations.append("*")
-                if event.unicode == u'+':
+                if event.key == pygame.K_KP_PLUS:
                     equations.append("+")
-                if event.unicode == u'-':
+                if event.key == pygame.K_KP_MINUS:
                     equations.append("-")
-                if event.unicode == u'/':
+                if event.key == pygame.K_KP_DIVIDE:
                     equations.append("/")
-                if event.unicode == u'.':
+                if event.key == pygame.K_KP_PERIOD:
                     equations.append(".")
-                if event.unicode == u'(':
+                if event.key == pygame.K_LEFTBRACKET:
                     equations.append("(")
-                if event.unicode == u')':
+                if event.key == pygame.K_RIGHTBRACKET:
                     equations.append(")")
-                if event.unicode == u'^':
+                if event.key == pygame.K_CARET:
                     equations.append("**")
 
                 #variable keys
@@ -157,6 +166,14 @@ def main():
 
                 elif event.key == pygame.K_RETURN:
                     active = False
+        
+        pygame.display.flip()
+
+        try:
+            result = eval(eqn)
+            DrawLine(result, 'h')
+        except Exception as e:
+            pass
 
 
 
